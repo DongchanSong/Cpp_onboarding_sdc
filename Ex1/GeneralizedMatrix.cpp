8fa2be2eeab58efd528c1111219c2f26a03f65eb
DongchanSong <dongchan.song@nearthlab.com>
2023-05-18 18:34:58 +0900

    Matrix dimension fault check update

--- Ex1/GeneralizedMatrix.cpp
+++ Ex1/GeneralizedMatrix.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <vector>
 
+int DimensionFaultCheck(std::vector<std::vector<int>>);
+
 class Matrix
 {
 public:
-    Matrix(int num_row, int num_col, std::vector<std::vector<int>> vect);
+    Matrix(int, int, std::vector<std::vector<int>>);
     int GetNumberOfRow();
     int GetNumberOfColumn();
-    void SetElement(int i, int j, int value);
+    void SetElement(int, int, int);
     void Display();
-    Matrix AddMatrices(Matrix B);
-    Matrix MultiplyMatrices(Matrix B);
+    Matrix AddMatrices(Matrix);
+    Matrix MultiplyMatrices(Matrix);
+
+    // int DimensionCheckFault(std::vector<std::vector<int>>);
 
 private:
     int numberOfRows_;
@@ -18,17 +22,28 @@ private:
     std::vector<std::vector<int>> matrix_;
 };
 
+
+
 int main()
 {
-    std::vector<std::vector<int>> m1{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-    std::vector<std::vector<int>> m2{{2, 0, 0, 1}, {0, 1, 0, 1}, {0, 0, 1, 1}};
+    // std::vector<std::vector<int>> m1{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    // std::vector<std::vector<int>> m2{{2, 0, 0, 1}, {0, 1, 0, 1}, {0, 0, 1, 1}};
+    std::vector<std::vector<int>> m1{{0},{0,0,1}};
+    std::vector<std::vector<int>> m2{{1}};
+
+    if (DimensionFaultCheck(m1) || DimensionFaultCheck(m2))
+    {
+        std::cout << "Error: Check dimension of matrix" << std::endl;
+        return 0;
+    }
 
     Matrix matrix1(m1.size(), m1[0].size(), m1);
     Matrix matrix2(m2.size(), m2[0].size(), m2);
 
+
+
     std::cout << "1. M1:" << std::endl;
     matrix1.Display();
-
     std::cout << "2. M2:" << std::endl;
     matrix2.Display();
 
@@ -56,6 +71,17 @@ int main()
                   << std::endl;
     }
 
+    return 0;
+}
+
+int DimensionFaultCheck(std::vector<std::vector<int>> vect) {
+    for (int i = 0; i < vect.size(); i++)
+    {
+        if (vect[i].size() != vect[0].size())
+        {
+            return 1;
+        }
+    }
     return 0;
 }
 
@@ -107,3 +133,4 @@ Matrix Matrix::MultiplyMatrices(Matrix B)
     }
     return Matrix(numberOfRows_, B.numberOfColumns_, MultipliedMatrix);
 }
+
