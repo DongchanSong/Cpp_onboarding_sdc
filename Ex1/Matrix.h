ab56830994c546900840c46b5e987078fadf828b
DongchanSong <dongchan.song@nearthlab.com>
2023-06-05 11:12:21 +0900

    클래스를 매개변수로 받는 경우  참조 변수 사용

--- Ex1/Matrix.h
+++ Ex1/Matrix.h
@@ -6,13 +6,13 @@
 class Matrix
 {
 public:
-    Matrix(const int, const int, const std::vector<std::vector<int>>);
+    Matrix(const int, const int, std::vector<std::vector<int>>);
     int GetNumberOfRow() const;
     int GetNumberOfColumn() const;
     void SetElement(const int, const int, const int);
     void Display() const;
-    Matrix AddMatrices(const Matrix) const;
-    Matrix MultiplyMatrices(const Matrix) const;
+    Matrix AddMatrices(Matrix &) const;
+    Matrix MultiplyMatrices(Matrix &) const;
 
 private:
     int numberOfRows_;
@@ -20,7 +20,7 @@ private:
     std::vector<std::vector<int>> matrix_;
 };
 
-Matrix::Matrix(const int num_row, const int num_col, const std::vector<std::vector<int>> vect)
+Matrix::Matrix(const int num_row, const int num_col, std::vector<std::vector<int>> vect)
 {
     numberOfRows_ = num_row;
     numberOfColumns_ = num_col;
@@ -50,7 +50,7 @@ void Matrix::Display() const
     }
 }
 
-Matrix Matrix::AddMatrices(const Matrix B) const
+Matrix Matrix::AddMatrices(Matrix &B) const
 {
 
     DimensionFaultCheck(matrix_, B.matrix_);
@@ -72,7 +72,7 @@ Matrix Matrix::AddMatrices(const Matrix B) const
         return Matrix(0, 0, std::vector<std::vector<int>>{{NULL}});
     }
 }
-Matrix Matrix::MultiplyMatrices(const Matrix B) const
+Matrix Matrix::MultiplyMatrices(Matrix &B) const
 {
     DimensionFaultCheck(matrix_, B.matrix_);
     if (numberOfColumns_ == B.numberOfRows_)
