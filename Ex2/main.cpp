84832c5a4a907e9a6714a027be2cbb04fe387c63
DongchanSong <dongchan.song@nearthlab.com>
2023-06-08 22:50:55 +0900

    주석 몇개 추가

--- Ex2/main.cpp
+++ Ex2/main.cpp
@@ -21,21 +21,26 @@ int main()
     // 일기토
     do
     {
+        // Move 덱으로부터 move number 가져오기
         int myMove = myMoveDeque.at(roundNumber - 1);
         int enemyMove = enemyMoveDeque.at(roundNumber - 1);
 
         DoNothingCheck(myDoNothingFlag, myMove, enemyMove);
         DoNothingCheck(enemyDoNothingFlag, enemyMove, myMove);
 
+        // 각자 행동 정의
         me->SetBehavior(myMove);
         enemy->SetBehavior(enemyMove);
 
+        // 위에서 정의된 행동으로 각자 피해 계산
         double myhurt = me->PerformBehavior(enemy);
         double enemyhurt = enemy->PerformBehavior(me);
 
+        // 피해 반영
         me->SetHealth(me->GetHealth() - myhurt);
         enemy->SetHealth(enemy->GetHealth() - enemyhurt);
 
+        // 라운드 결과 출력
         std::cout << "\nRound " << roundNumber << ") You: " << ToString(myMove) << ", Enemy: " << ToString(enemyMoveDeque.at(roundNumber - 1)) << "\n";
         PrintHealth(me->GetHealth(), enemy->GetHealth());
 
