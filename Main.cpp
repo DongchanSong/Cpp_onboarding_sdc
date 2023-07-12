12c505f622f7c380e789a3157b0e5de46553e4d4
DongchanSong <dongchan.song@nearthlab.com>
2023-07-12 14:33:02 +0900

    Smart pointer 적용

--- Main.cpp
+++ Main.cpp
@@ -1,28 +1,39 @@
 #include "FcInputImpl.h"
+#include <memory>
 
 int main()
 {
-    FcInputImpl *fcM300 = new FcInputImpl_M300();
-    VehicleData *dataM300 = fcM300->FromApiToMc();
+    // FcInputImpl *fcM300 = new FcInputImpl_M300();
+    // VehicleData *dataM300 = fcM300->FromApiToMc();
 
-    FcInputImpl *fcM600 = new FcInputImpl_M600();
-    VehicleData *dataM600 = fcM600->FromApiToMc();
+    // FcInputImpl *fcM600 = new FcInputImpl_M600();
+    // VehicleData *dataM600 = fcM600->FromApiToMc();
 
-    FcInputImpl *fcMavic = new FcInputImpl_Mavic();
-    VehicleData *dataMavic = fcMavic->FromApiToMc();
+    // FcInputImpl *fcMavic = new FcInputImpl_Mavic();
+    // VehicleData *dataMavic = fcMavic->FromApiToMc();
 
-    FcInputImpl *fcSkydio = new FcInputImpl_Skydio();
-    VehicleData *dataSkydio = fcSkydio->FromApiToMc();
+    // FcInputImpl *fcSkydio = new FcInputImpl_Skydio();
+    // VehicleData *dataSkydio = fcSkydio->FromApiToMc();
 
-    delete fcM300;
-    delete fcM600;
-    delete fcMavic;
-    delete fcSkydio;
+    // delete fcM300;
+    // delete fcM600;
+    // delete fcMavic;
+    // delete fcSkydio;
 
-    delete dataM300;
-    delete dataM600;
-    delete dataMavic;
-    delete dataSkydio;
+    // delete dataM300;
+    // delete dataM600;
+    // delete dataMavic;
+    // delete dataSkydio;
+
+    std::shared_ptr<FcInputImpl> fcM300(new FcInputImpl_M300());
+    std::shared_ptr<FcInputImpl> fcM600(new FcInputImpl_M600());
+    std::shared_ptr<FcInputImpl> fcMavic(new FcInputImpl_Mavic());
+    std::shared_ptr<FcInputImpl> fcSkydio(new FcInputImpl_Skydio());
+
+    std::unique_ptr<VehicleData> dataM300(fcM300->FromApiToMc());
+    std::unique_ptr<VehicleData> dataM600(fcM600->FromApiToMc());
+    std::unique_ptr<VehicleData> dataMavic(fcMavic->FromApiToMc());
+    std::unique_ptr<VehicleData> dataSkydio(fcSkydio->FromApiToMc());
 
     return 0;
 }
--- myTest.cpp
+++ myTest.cpp
@@ -1,17 +1,15 @@
 #include <gtest/gtest.h>
 #include "FcInputImpl.h"
 
-FcInputImpl *fcM300 = new FcInputImpl_M300();
-VehicleData *dataM300 = fcM300->FromApiToMc();
+std::shared_ptr<FcInputImpl> fcM300(new FcInputImpl_M300());
+std::shared_ptr<FcInputImpl> fcM600(new FcInputImpl_M600());
+std::shared_ptr<FcInputImpl> fcMavic(new FcInputImpl_Mavic());
+std::shared_ptr<FcInputImpl> fcSkydio(new FcInputImpl_Skydio());
 
-FcInputImpl *fcM600 = new FcInputImpl_M600();
-VehicleData *dataM600 = fcM600->FromApiToMc();
-
-FcInputImpl *fcMavic = new FcInputImpl_Mavic();
-VehicleData *dataMavic = fcMavic->FromApiToMc();
-
-FcInputImpl *fcSkydio = new FcInputImpl_Skydio();
-VehicleData *dataSkydio = fcSkydio->FromApiToMc();
+std::unique_ptr<VehicleData> dataM300(fcM300->FromApiToMc());
+std::unique_ptr<VehicleData> dataM600(fcM600->FromApiToMc());
+std::unique_ptr<VehicleData> dataMavic(fcMavic->FromApiToMc());
+std::unique_ptr<VehicleData> dataSkydio(fcSkydio->FromApiToMc());
 
 void RangeCheck(int, int, int);
 void RangeCheck(float, float, float);
